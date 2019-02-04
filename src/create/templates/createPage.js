4afa101b17e35bb5ba7ce3e5528bc9e82cf3d3c3
nmchenry01 <nicholas.mchenry@levvel.io>
2019-02-03 18:16:34 -0800

    feat(html) slight change to the /create html

--- src/create/templates/createPage.js
+++ src/create/templates/createPage.js
@@ -2,7 +2,8 @@ const generateCreatePage = (submittedURL, link) => {
   return `
   <html>
     <body>
-        <h3>The URL : "${submittedURL}" has been shortened to ${link}
+        <a href="${submittedURL}">${link}</a>
+        <h3>The URL : "${submittedURL}" has been shortened to <a href="${submittedURL}">${link}</a>
         </h3>
     </body>
    </html>
